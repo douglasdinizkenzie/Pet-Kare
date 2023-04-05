485e186119ce92cba80f1b55914b1f0c98bc38e7
Douglas <douglasslycooper@gmail.com>
2023-04-04 23:29:36 -0300

    feat: tarefa 3, create list routes with pagination

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import status, Request, Response, APIView
+from pets.models import Pet
+from rest_framework.pagination import PageNumberPagination
+from pets.serializers import PetSerializer
 
-# Create your views here.
+
+class PetView(APIView, PageNumberPagination):
+    def get(self, request: Request) -> Response:
+        pets = Pet.objects.all()
+        result_pages = self.paginate_queryset(pets, request, view=self)
+        serializer = PetSerializer(result_pages, many=True)
+        return self.get_paginated_response(serializer.data)
+
+    def post(self, request: Request) -> Response:
+        ...
